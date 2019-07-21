fc0315a556746e28e4543b8592a7270e2cc4c36e
Santhosh <santhoshkumarr1096@gmail.com>
2019-07-21 22:04:13 +0530

    Added defaultMW

---
+++ defaultMW.go
@@ -0,0 +1,33 @@
+package mint
+
+import (
+	"time"
+)
+
+//LoggerMW logger middleware
+func loggerMW(c *Context) {
+	start := time.Now()
+	path := c.Request.URL.Path
+	c.Next()
+	log := new(Logger)
+	log.TimeStamp = time.Now()
+	log.Latency = log.TimeStamp.Sub(start)
+	log.Method = c.Method
+	log.StatusCode = c.StatusCode
+	log.ClientIP = c.ClientIP()
+	log.BodySize = c.size
+	log.Path = path
+	log.Errors = c.Error
+	log.Print()
+}
+
+//CustomHeaders parse the headers of request and set Corrosponding context value
+func customHeadersMW(c *Context) {
+	cr := c.Request.Header.Get("X-CR")
+	if cr == "1" {
+		c.CR = true
+	} else {
+		c.CR = false
+	}
+	c.Next()
+}
